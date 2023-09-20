e61ab94ab22f2e38feee3fff63526ed201b04b3c
Kamil Malkowski <kamil.malkowski96@gmail.com>
2023-09-20 18:33:39 +0200

    Created models: Users, Images

--- recurimentTask/api/models.py
+++ recurimentTask/api/models.py
@@ -1,3 +1,20 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.CharField(max_length=100, null=False)
+    password = models.CharField(max_length=100, null=False)
+    plan = models.CharField(max_length=100, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Images(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    image_path = models.FileField()
+    image_format = models.CharField(max_length=100)
+
--- recurimentTask/recurimentTask/settings.py
+++ recurimentTask/recurimentTask/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+from decouple import config
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-exhj^h*=%ynm%(390877^9=v&lg@1r!kx&+su)vn-2oaiw93g('
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,7 +83,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "db",
         "USER": "admin",
-        "PASSWORD": "admin",
+        "PASSWORD": config('PASSWORD'),
         "HOST": "localhost",
         "PORT": "5432",
     }
@@ -104,8 +106,8 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
     'SLIDING_TOKEN_LIFETIME': timedelta(days=1),
     'SLIDING_TOKEN_REFRESH_OFFSET': timedelta(days=0.5),
-    'SIGNING_KEY': 'thisissecret',
-    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': config('SIGNING_KEY'),
+    'ALGORITHM': config('ALGORITHM'),
     'USER_ID_FIELD': 'id',
 }
 
