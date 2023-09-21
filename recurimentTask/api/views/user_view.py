53355323bb6f0deb339a926fc8f803d0a9f2969e
Kamil Malkowski <kamil.malkowski96@gmail.com>
2023-09-21 18:48:31 +0200

    Added links to all user photos

---
+++ recurimentTask/api/views/user_view.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.hashers import check_password
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
+from ..repositories.user_repository import UserRepository
+
+
+@api_view(['POST'])
+def login(request):
+    email = request.data.get("email")
+    password = request.data.get("password")
+
+    if not email or not password:
+        return Response({"error": "Email and password are required."}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = UserRepository.get_user_by_email(email)
+
+    if not user:
+        return Response({"error": "User does not exist."}, status=status.HTTP_404_NOT_FOUND)
+
+    if not check_password(password, user.password):
+        return Response({"error": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
+
+    refresh = RefreshToken.for_user(user)
+
+    return Response(
+        {
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
+        },
+        status=status.HTTP_200_OK,
+    )
+
+
+
+
+
+
+
+
